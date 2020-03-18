727c86c8f2b429159ad20749733a8d4253951c12
Federico Meini <fme@templafy.com>
2020-03-18 22:44:44 +0100

    Add clustering_on_reduced_data script

---
+++ clustering_on_reduced_data.py
@@ -0,0 +1,20 @@
+import clustering
+import data
+
+
+def run_clustering():
+    datasets = ['fashion', 'wine']
+    versions = ['ica', 'pca', 'rp', 'svd']
+
+    for dataset in datasets:
+        for version in versions:
+            print(f'Running clustering on {datasets} ({version} version)')
+            data.load_data(dataset, version)
+            clustering.kmeans_kselection(dataset, version)
+            clustering.kmeans_evaluation(dataset, version)
+            clustering.em_kselection(dataset, version)
+            clustering.em_evaluation(dataset, version)
+
+
+if __name__ == '__main__':
+    run_clustering()
